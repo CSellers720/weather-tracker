a092b41a11791acc54a23889e0c829d4ef344546
CSellers720 <charliejsellers@gmail.com>
2021-03-05 15:24:38 -0600

    Relocated config & example from db to /
    I added the API_KEY to the config file and decided it would make
    more sense to place it in the project root (as opposed to /db/
    where it was located previously).

---
+++ config.example.js
@@ -0,0 +1,12 @@
+/*
+Fill in the empty fields and rename file "config.js"
+*/
+
+const FILL_ME_IN = null;
+
+module.exports = {
+  USERNAME: FILL_ME_IN,
+  PASSWORD: FILL_ME_IN,
+  CLUSTER_NAME: FILL_ME_IN,
+  API_KEY: FILL_ME_IN,
+};
