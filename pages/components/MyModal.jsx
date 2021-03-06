ff3a473a4aee814d807aaa5f068a8db8fc2afed2
CSellers720 <charliejsellers@gmail.com>
2021-03-06 14:03:04 -0600

    Created a modal with a form and styled it
    It's still just a placeholder form, but I'll get to that soon.

---
+++ pages/components/MyModal.jsx
@@ -0,0 +1,22 @@
+import Modal from 'react-bootstrap/Modal';
+import MyForm from './MyForm';
+import styles from '../../styles/Modal.module.css';
+
+const MyModal = ({ show, onHide }) => (
+  <Modal
+    className={styles.custom_modal}
+    show={show}
+    onHide={onHide}
+    size="lg"
+    centered
+  >
+    <Modal.Header className={styles.header}>
+      <Modal.Title>Track Local Weather</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <MyForm />
+    </Modal.Body>
+  </Modal>
+);
+
+export default MyModal;
