5ac2571638a91673e2812364a2831a4f3f0a09ba
CSellers720 <charliejsellers@gmail.com>
2021-03-10 17:19:46 -0600

    Changed config layout to make it easier to use
    Instead of exporting individual credentials, config now exports
    full MongoDB URI, simplifying use.

--- config.example.js
+++ config.example.js
@@ -4,10 +4,13 @@ Fill in the empty fields and rename file "config.js"
 
 const FILL_ME_IN = null;
 
+const USERNAME = FILL_ME_IN;
+const PASSWORD = FILL_ME_IN;
+const CLUSTER_NAME = FILL_ME_IN;
+const MONGO_URL = `mongodb+srv://${USERNAME}:${PASSWORD}@${CLUSTER_NAME}.mongodb.net/weather`
+
 module.exports = {
-  USERNAME: FILL_ME_IN,
-  PASSWORD: FILL_ME_IN,
-  CLUSTER_NAME: FILL_ME_IN,
+  MONGO_URL,
   API_KEY: FILL_ME_IN,
   GOOGLE_API_KEY: FILL_ME_IN,
 };
