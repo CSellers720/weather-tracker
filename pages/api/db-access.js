becf0f6063479b317982be313b72150b087c1746
CSellers720 <charliejsellers@gmail.com>
2021-03-11 13:00:15 -0600

    Added an api endpoint for db access

---
+++ pages/api/db-access.js
@@ -0,0 +1,20 @@
+import connectDB from '../../middleware/mongodb';
+import Track from '../../models/track';
+
+const handler = async (req, res) => {
+  if (req.method === 'GET') {
+    Track
+      .find({})
+      .sort({ time: -1 })
+      .limit(req.body?.limit ?? 0)
+      .exec((error, data) => {
+        if (error) {
+          res.status(400).json({ error });
+        } else {
+          res.status(200).json({ data });
+        }
+      });
+  }
+};
+
+export default connectDB(handler);
