41afdf6d004e25eaeca0829b031f456e87546545
CSellers720 <charliejsellers@gmail.com>
2021-03-12 00:49:42 -0600

    Extracted parts into their own components

--- pages/tableview.jsx
+++ pages/tableview.jsx
@@ -2,15 +2,8 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
+import { Blurb, Headers, Row } from './components/tableview';
 import styles from '../styles/TableView.module.css';
-
-const pad = (input) => {
-  const string = input.toString();
-  if (string.length === 1) {
-    return `0${string}`;
-  }
-  return string;
-};
 
 const TableView = () => {
   const [tableData, setTableData] = useState();
@@ -25,81 +18,18 @@ const TableView = () => {
   if (tableData) {
     return (
       <Container fluid className={styles.container}>
-        <h4>Temperature data is in Celsius</h4>
+        <Blurb />
         <Table
           variant="weather"
           size="sm"
           striped
           bordered
         >
-          <thead>
-            <tr>
-              <th>Date/Time</th>
-              <th>Location</th>
-              <th>Description</th>
-              <th>Temperature</th>
-              <th>Pressure</th>
-              <th>Humidity</th>
-              <th>Wind</th>
-            </tr>
-          </thead>
+          <Headers />
           <tbody>
-            {tableData.map(({
-              main, weather, wind, location, time,
-            }) => {
-              const date = new Date();
-              date.setTime(time);
-              return (
-                <tr key={date.getTime()}>
-                  <td>
-                    {date.toDateString()}
-                    <br />
-                    {`${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`}
-                  </td>
-                  <td>
-                    {location.city}
-                    <br />
-                    {location.state}
-                    <br />
-                    {location.country}
-                  </td>
-                  <td className={styles.iconTd}>
-                    <img className={styles.iconImg} alt="Weather Icon" src={`http://openweathermap.org/img/wn/${weather.icon}.png`} />
-                    <div className={styles.main}>
-                      {weather.main}
-                    </div>
-                    <div className={styles.desc}>
-                      {weather.description}
-                    </div>
-                  </td>
-                  <td>
-                    At listed time:
-                    {`  ${main.current}°`}
-                    <br />
-                    High:
-                    {`  ${main.max}°`}
-                    <br />
-                    Low:
-                    {`  ${main.min}°`}
-                    <br />
-                  </td>
-                  <td>
-                    {`${main.pressure} hPa`}
-                  </td>
-                  <td>
-                    {main.humidity}
-                    %
-                  </td>
-                  <td>
-                    {`${wind.speed} `}
-                    m/sec
-                    <br />
-                    {`${wind.deg}  `}
-                    degrees
-                  </td>
-                </tr>
-              );
-            })}
+            {tableData.map((data) => (
+              <Row rowData={data} key={data.time} />
+            ))}
           </tbody>
         </Table>
       </Container>
