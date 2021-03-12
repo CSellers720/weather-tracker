1a6ee9e85bd013e01bc34a9696c58202bf089511
CSellers720 <charliejsellers@gmail.com>
2021-03-12 00:47:23 -0600

    Added async/await to ensure async operations finish

--- pages/api/fetch-data.js
+++ pages/api/fetch-data.js
@@ -2,17 +2,17 @@
 import axios from 'axios';
 import { API_KEY } from '../../config';
 import connectDB from '../../middleware/mongodb';
-import genTrack from './helpers/genTrack';
-import upsertLocation from './helpers/upsertLocation';
+import genTrack from '../../helpers/genTrack';
+import upsertLocation from '../../helpers/upsertLocation';
 
 const prefix = 'https://api.openweathermap.org/data/2.5/weather';
 
-const handler = async (req, res) => {
+const handler = (req, res) => {
   if (req.method === 'POST') {
     const { city, state, country } = req.body;
     const locationString = `${city},${state},${country}`;
-    await axios.get(`${prefix}?q=${locationString}&units=metric&appid=${API_KEY}`)
-      .then((results) => {
+    axios.get(`${prefix}?q=${locationString}&units=metric&appid=${API_KEY}`)
+      .then(async (results) => {
         const { data } = results;
         data.location = { city, state, country };
         const newTrack = genTrack(data);
