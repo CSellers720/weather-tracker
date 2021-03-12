447c81143d486e8528b48203d201c0924984ab83
CSellers720 <charliejsellers@gmail.com>
2021-03-12 00:44:42 -0600

    Replaced placeholder with a chart.

--- pages/chartview.jsx
+++ pages/chartview.jsx
@@ -1,11 +1,76 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-underscore-dangle */
 import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Line, defaults } from 'react-chartjs-2';
 import Container from 'react-bootstrap/Container';
-import styles from '../styles/TableView.module.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import createDataObject from '../helpers/createDataObject';
+import styles from '../styles/ChartView.module.css';
+
+defaults.global.defaultFontColor = '#FFFFFF';
+
+// const options = {
+//   scales: {
+//     yAxes: [
+//       {
+//         display: true,
+//         ticks: {
+//           beginAtZero: false,
+//         },
+//       },
+//     ],
+//   },
+// };
 
 const ChartView = () => {
+  const [chartData, setChartData] = useState();
+  const [selected, setSelected] = useState(0);
+
+  useEffect(() => {
+    axios.get('/api/db-access', { limit: 3 })
+      .then(({ data }) => {
+        const formattedChartData = [];
+        data.data.forEach(({ hourly, location, _id }) => {
+          formattedChartData.push({
+            hourly,
+            location,
+            id: _id,
+          });
+        });
+        setChartData(formattedChartData);
+        console.log(formattedChartData);
+      })
+      .catch((err) => {
+        console.error('Chart view unable to access database', err);
+      });
+  }, []);
+
+  if (chartData) {
+    const { city, state, country } = chartData[selected].location;
+    return (
+      <Container className={styles.container}>
+        <br />
+        <h1>{`${city}, ${state}, ${country}`}</h1>
+        <Dropdown>
+          <Dropdown.Toggle>
+            Select a location here
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            {chartData.map(({ location }, index) => (
+              <Dropdown.Item eventKey={index} onClick={() => setSelected(index)}>
+                {`${location.city}, ${location.state}, ${location.country}`}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+        <Line data={createDataObject(chartData[selected])} />
+      </Container>
+    );
+  }
   return (
     <Container fluid className={styles.container}>
-      <h1>This is the chart view</h1>
+      <h1>Loading</h1>
     </Container>
   );
 };
